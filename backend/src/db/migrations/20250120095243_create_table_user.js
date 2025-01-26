652c29b4c753e70b19311cfaf713a8e4fc79c967
nav singh <navtej927@gmail.com>
2025-01-26 00:28:28 +0000

    update seed

--- backend/src/db/migrations/20250120095243_create_table_user.js
+++ backend/src/db/migrations/20250120095243_create_table_user.js
@@ -18,20 +18,24 @@ exports.up = function (knex) {
     knex.schema.createTable(TableNames.todo, (table) => {
       table.increments("id").primary();
       table.string("title").notNullable();
-      table.string("desc")();
+      table.string("desc");
       table.boolean("isCompleted");
       table.dateTime("deadline");
       table.timestamps(true, true);
+      table.integer("user_id").unsigned().notNullable();
       table.foreign("user_id").references("id").inTable(TableNames.user).onDelete("CASCADE");
     }),
     knex.schema.createTable(TableNames.comment, (table) => {
       table.increments("id").primary();
-      table.string("desc")();
+      table.string("desc");
       table.integer("thumbs-up").defaultTo(0);
       table.integer("thumbs-down").defaultTo(0);
       table.timestamps(true, true);
+      table.integer("user_id").unsigned().notNullable();
       table.foreign("user_id").references("id").inTable(TableNames.user).onDelete("CASCADE");
-      table.foreign("todo_id").references("id").inTable(TableNames.todo).onDelete("CASCADE");
+
+      table.integer("todo_id").unsigned().notNullable();
+      table.foreign("todo_id").references("id").inTable(TableNames.user).onDelete("CASCADE");
     })
   ])
 };
